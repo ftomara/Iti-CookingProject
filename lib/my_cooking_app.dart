b38456b6926c594c15483df801b6c8339f16b2d8
ftomara <ftomara647@gmail.com>
2024-09-02 15:59:48 +0300

    added screenutil widget

--- lib/my_cooking_app.dart
+++ lib/my_cooking_app.dart
@@ -1,5 +1,6 @@
 import 'package:cooking_app/features/home/ui/screens/on_boarding.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyCookingApp extends StatelessWidget {
   const MyCookingApp({super.key});
@@ -7,9 +8,10 @@ class MyCookingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-     
-      home: OnBoarding(),
+    return const ScreenUtilInit(
+      child: MaterialApp(
+        home: OnBoarding(),
+      ),
     );
   }
 }
