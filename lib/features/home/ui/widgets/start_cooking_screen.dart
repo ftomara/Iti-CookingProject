143c53da7ab1547923c62bc39faa9dfa1e3f3597
NourhanOElsayed <nourhanusama1602@gmail.com>
2024-09-05 11:29:57 +0300

    fixing overflow error in start cooking screen

--- lib/features/home/ui/widgets/start_cooking_screen.dart
+++ lib/features/home/ui/widgets/start_cooking_screen.dart
@@ -14,58 +14,54 @@ class StartCookingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Center(
-        child: Column(
-          children: [
-            const BackButtonAppbar(),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  // right: 18,
-                  // left: 18,
-                  top: 60,
-                  bottom: 160,
+      child: Column(
+        children: [
+          const BackButtonAppbar(),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 60,
+                bottom: 80,
+              ),
+              child: Container(
+                width: 352,
+                // height: double.infinity,
+                decoration: BoxDecoration(
+                  color: MyColors.orangecolor,
+                  borderRadius: BorderRadius.circular(24),
                 ),
-                child: Container(
-                  width: 352,
-                  height: 572,
-                  decoration: BoxDecoration(
-                    color: MyColors.orangecolor,
-                    borderRadius: BorderRadius.circular(24),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 55,
+                    vertical: 64,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 55,
-                      vertical: 64,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          '''Hey!
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '''Hey!
 Let’s Enjoy Some Cookin’''',
-                          style: GoogleFonts.oswald(
-                            color: MyColors.butterycolor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 36,
-                          ),
-                          textAlign: TextAlign.center,
+                        style: GoogleFonts.oswald(
+                          color: MyColors.butterycolor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 36,
                         ),
-                        Lottie.asset(
-                            'assets/animations/lets_cook_animation.json'),
-                        const Spacer(),
-                        AnimatedButton(
-                          pageController: pageController,
-                        ),
-                      ],
-                    ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Lottie.asset(
+                          'assets/animations/lets_cook_animation.json'),
+                      const Spacer(),
+                      AnimatedButton(
+                        pageController: pageController,
+                      ),
+                    ],
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
